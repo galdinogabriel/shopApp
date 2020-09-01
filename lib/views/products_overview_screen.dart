e962b673e94501b8b1d3bf46aa800c22846b1d57
Gabriel Matos <matosgabriel017@gmail.com>
2020-09-01 10:09:58 -0300

    create component product grid create paste 'data' with mockups data

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+import '../data/dummy_data.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minha loha"),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) {
+          return Text(loadedProducts[i].title);
+        },
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
   }
 }
